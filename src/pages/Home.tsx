cb417442a3bcd6aff098006a3ac36909f2fadc3c
Clifford Goh <cliffordgoh25@gmail.com>
2023-10-18 01:32:26 +0800

    added user profile page
    still in Home.tsx - to do routing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { Container } from "react-bootstrap"
 import { useUserAuth } from "../hooks/useUserAuth"
+import './Home.css'
 
 export const Home = () => {
     const { user } = useUserAuth()
@@ -11,11 +12,46 @@ export const Home = () => {
     }
 
     return <Container>
-        <p>user.id: {user.id}</p>
-        <p>user.username: {user?.username}</p>
-        <p>user.name: {user?.name}</p>
-        <p>user.email: {user?.email}</p>
-        <p>user.contactNo: {user.contactNo}</p>
-        <p>user.role: {user?.role}</p>
+        <div className="user-profile">
+            <h1>User Profile</h1>
+            <div className="user-info">
+                <img
+                    src="holder.js/100px180" // Add a profile picture here
+                    className="user-avatar"
+                    onError={({ currentTarget }) => {
+                        currentTarget.onerror = null; // prevents looping
+                        currentTarget.src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg"
+                    }}
+                />
+                <table className="user-table">
+                    <tbody>
+                    <tr>
+                        <td><strong>User ID:</strong></td>
+                        <td>{user.id}</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Username:</strong></td>
+                        <td>{user.username}</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Name:</strong></td>
+                        <td>{user.name}</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Email:</strong></td>
+                        <td>{user.email}</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Contact Number:</strong></td>
+                        <td>{user.contactNo}</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Role:</strong></td>
+                        <td>{user.role}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </Container>
 }
