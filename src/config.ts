2e1b1062dae9ce28502a9a14dda4ae5712253e56
DEVNET\d1351188 <shenzi.xin@ncs.com.sg>
2023-10-19 17:35:26 +0800

    add config

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ const Config = {
         Product: API_GATEWAY ? `https://${API_GATEWAY}/product-service` : 'http://localhost:9002',
         Cart: API_GATEWAY ? `https://${API_GATEWAY}/cart-service` : 'http://localhost:9003',
         Order: API_GATEWAY ? `https://${API_GATEWAY}/order-service` : 'http://localhost:9004',
-        Inventory: 'http://localhost:8080',
+        Inventory: API_GATEWAY ? `https://${API_GATEWAY}/inventory-service` :'http://localhost:8080',
     }
 }
 
